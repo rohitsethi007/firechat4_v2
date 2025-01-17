bf644c06a1c5facfb0d7e7f8c9454289e6bcf353
Rohit Sethi <rohitsethi@Rohits-MacBook-Pro.local>
2025-01-17 23:00:15 +0000

    post page event in progress addedby section missing - stable

--- src/app/new-event/new-event.page.ts
+++ src/app/new-event/new-event.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl, FormArray } from '@angular/forms';
 import { ActionSheetController, NavController } from '@ionic/angular';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DataService } from '../services/data.service';
@@ -13,6 +13,25 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import * as moment from 'moment';
 import { CheckboxCheckedValidator } from '../validators/checkbox-checked.validator';
 
+interface Event {
+  groupId: string;
+  groupName: string;
+  date: Date;
+  time: string;
+  title: string;
+  location: string;
+  videoLink?: string;
+  bannerImage?: string;
+  createdAt: Date;
+  isAttending: boolean;
+}
+
+interface Group {
+  name: string;
+  posts: string[];
+  // other properties
+}
+
 @Component({
   selector: 'app-new-event',
   templateUrl: './new-event.page.html',
@@ -23,7 +42,6 @@ export class NewEventPage implements OnInit {
   private event: any;
   private user: any;
   private eventForm: FormGroup;
-  private postTags: any = [];
   private groupId: any;
   private group: any;
   private eventId: any;
@@ -39,25 +57,9 @@ export class NewEventPage implements OnInit {
   groups: any;
   userNotifications: any = [];
   private postMedia: any = [];
-
-  validations = {
-    title: [
-      { type: 'required', message: 'Title is a required field.' }
-    ],
-    description: [
-      { type: 'required', message: 'Description is a required field.' }
-    ],
-    eventDate: [
-      { type: 'required', message: 'Please select the Event date.' }
-    ],
-    eventTime: [
-      { type: 'required', message: 'Please select the Event time.' }
-    ],
-    tags: [
-      { type: 'required', message: 'Please select at least one tag.' }
-    ]
-    };
-
+  // In your component class
+  bannerImage: string | null = null;
+  
   constructor(
     private route: ActivatedRoute,
     private router: Router,
@@ -68,7 +70,8 @@ export class NewEventPage implements OnInit {
     public camera: Camera,
     public actionSheet: ActionSheetController,
     public imageProvider: ImageService,
-    public firestore: AngularFirestore
+    public firestore: AngularFirestore,
+    public formBuilder: FormBuilder
   ) {
     this.groupId = this.route.snapshot.params.id;
 
@@ -78,17 +81,6 @@ export class NewEventPage implements OnInit {
     } else {
       this.step = 2;
     }
-
-    this.eventForm = new FormGroup({
-        title: new FormControl('', Validators.required),
-        description: new FormControl('', Validators.required),
-        eventDate: new FormControl('', Validators.required),
-        eventTime: new FormControl('', Validators.required),
-        address: new FormControl('', Validators.required),
-        tags: new FormArray([], CheckboxCheckedValidator.tagsSelected(1)),
-        eventAttending: new FormControl(''),
-        weblink: new FormControl('')
-      });
 
     // Set min and max event dates
     const dateObj = new Date();
@@ -102,6 +94,16 @@ export class NewEventPage implements OnInit {
    ionViewDidEnter() {}
 
    ngOnInit() {
+    this.eventForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      eventDate: ['', Validators.required],
+      eventTime: ['', Validators.required],
+      location: ['', Validators.required],
+      videoLink: [''], // Optional field,
+      isAttending: [false],
+      groupId: ['']  
+    });
+
     this.dataProvider.getCurrentUser().then((u) => {
       u.snapshotChanges().subscribe((value: any) => {
         this.user = value.payload.data();
@@ -111,22 +113,6 @@ export class NewEventPage implements OnInit {
         addedByImg: value.payload.data().img
       };
   
-        // Initialize
-        this.event = { 
-          addedByUser: this.addedByUser,
-          date: '',
-          title: '',
-          postTags : [],
-          groupId: '',
-          groupName: '',
-          type: 'event',
-          data: {},
-          totalReactionCount: 0,
-          totalReviewCount: 0,
-          postMedia: []
-      };
-  
-      
         if (this.step === 1) {
         this.title = 'Select a group ...';
         // Get User Groups List
@@ -144,14 +130,10 @@ export class NewEventPage implements OnInit {
           });
           }
         } else {
-          this.title = 'Poll';
+          this.title = 'Event';
     
           this.dataProvider.getGroup(this.groupId).snapshotChanges().subscribe((group) => {
             this.group = group.payload.data();
-            this.group.groupTags.forEach((element: any) => {
-              this.postTags.push({val: element, isChecked: false});
-            });
-            this.addTagControls();
           });
         }
       });
@@ -176,137 +158,72 @@ export class NewEventPage implements OnInit {
     }
   }
 
-  addTagControls() {
-    this.postTags.forEach((o, i) => {
-      const control = new FormControl(i === 0); // if first item set to true, else false
-      (this.eventForm.controls.tags as FormArray).push(control);
-    });
-  }
-
-   submitEventForm() {
+  submitEventForm() {
     this.loadingProvider.show();
+    console.info('new group id', this.groupId);
 
-    // Add event info and date.
-    this.event.groupId = this.groupId;
-    this.event.groupName = this.group.name;
-    this.event.date = new Date();
-    this.event.title = this.eventForm.value.title;
-    this.event.postTags = [];
-    this.event.postTags = this.postTags;
-    this.event.postMedia = this.postMedia;
+    // Add event info and date
+    this.event = {
+      ...this.event,  // preserve any existing event properties
+      groupId: this.groupId,
+      groupName: this.group.name,
+      date: this.eventForm.value.eventDate,
+      time: this.eventForm.value.eventTime,
+      title: this.eventForm.value.title,
+      location: this.eventForm.value.location,
+      videoLink: this.eventForm.value.videoLink || null,
+      bannerImage: this.bannerImage || './assets/images/default-banner.jpg',
+      createdAt: new Date(),
+      isAttending: this.eventForm.value.isAttending
+    };
 
-    this.event.data = {
-      message: this.eventForm.value.description,
-      eventDate: this.eventForm.value.eventDate,
-      eventTime: moment(this.eventForm.value.eventTime).format('h:mm A'),
-      address: this.eventForm.value.address,
-      link: this.eventForm.value.weblink
-      }
-
-
-    // Add event to database.
+    // Add event to database
     this.dataProvider.addPost(this.event).then((success) => {
-        const eventId = success.id;
-        this.eventId = eventId;
-        if (this.group.posts  === undefined) {
-          this.group.posts = [];
-        }
-        this.group.posts.push(this.eventId);
-        // Update group data on the database.
-        this.dataProvider.getGroup(this.groupId).update({
-          posts: this.group.posts
-        });
-        // Update the Checkin data for this event
-        if ( this.eventForm.value.eventAttending === true) {
-          const reaction = {
-            addedByUser: this.addedByUser,
-            dateCreated: new Date(),
-            reactionType: 'Checkin'
-          };
-          this.dataProvider.updatePostReactions(eventId, reaction);
-        }
+      const eventId = success.id;
+      this.eventId = eventId;
 
-        this.loadingProvider.hide();
-        this.router.navigateByUrl('tabs/tab3');
+      // Initialize posts array if undefined
+      if (this.group.posts === undefined) {
+        this.group.posts = [];
+      }
+      this.group.posts.push(this.eventId);
+
+      // Update group data on the database
+      this.dataProvider.getGroup(this.groupId).update({
+        posts: this.group.posts
       });
 
-   }
+      // Update the Checkin data for this event
+      if (this.eventForm.value.isAttending === true) {
+        const reaction = {
+          addedByUser: this.addedByUser,
+          dateCreated: new Date(),
+          reactionType: 'Checkin'
+        };
+        this.dataProvider.updatePostReactions(eventId, reaction);
+      }
 
-   linkFocusOut() {
-    this.loadingProvider.show();
-    const getMeta = require('lets-get-meta');
-    this.http.get(
-      this.eventForm.value.weblink,
-      {responseType: 'text'}
-      )
-    .subscribe(res => {
-      const o = getMeta(res);
-      this.linkDescription = o.description;
       this.loadingProvider.hide();
+      this.router.navigateByUrl('/app/tabs/tab1');
+    }).catch(error => {
+      console.error('Error creating event:', error);
+      this.loadingProvider.hide();
+      // Add error handling here if needed
     });
+}
 
-    // const o = getMeta("<meta name='page' content='index'><meta property='description' content='This is the index page'>")
-    // this.linkDescription = o.description;
-    }
 
     selectGroup(groupId) {
       this.groupId = groupId;
       console.log('groupId', groupId);
       this.step = 2;
-      this.title = 'Event';
+      this.title = 'New Event';
   
       this.dataProvider.getGroup(this.groupId).snapshotChanges().subscribe((group) => {
         this.group = group.payload.data();
-        this.group.groupTags.forEach((element: any) => {
-          this.postTags.push({val: element, isChecked: false});
-        });
-        this.addTagControls();
     });
      }
-
-     attach() {
-      this.actionSheet.create({
-        header: 'Attach images',
-        buttons: [{
-          text: 'Camera',
-          handler: () => {
-            this.imageProvider.uploadPostPhoto(this.camera.PictureSourceType.CAMERA).then((url) => {
-              this.postMedia.push(url);
-              console.log(url);
-            });
-          }
-        }, {
-          text: 'Photo Library',
-          handler: () => {
-            this.imageProvider.getImages().then((url) => {
-              this.postMedia = this.postMedia.concat(url);
-              console.log(url);
-            });
-          }
-        },
-        {
-          text: 'Video',
-          handler: () => {
-            this.imageProvider.uploadPostVideo().then(url => {
-              this.postMedia(url);
-              console.log(url);
-            });
-          }
-        }
-          ,  {
-          text: 'Cancel',
-          role: 'cancel',
-          handler: () => {
-            console.log('cancelled');
-          }
-        }]
-      }).then(r => r.present());
-    }
-  
-    removeMedia(media) {
-      this.postMedia.splice();
-      this.postMedia = this.postMedia.filter(x => x !== media);
-      console.log('this.postMedia:', this.postMedia);
-      this.imageProvider.deletePostPhoto(media);
+    uploadBanner() {
+      // Implement your image upload logic here
     }
 }
