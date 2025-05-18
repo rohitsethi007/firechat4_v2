206b504764317be21816db4d949d278e72b0b50a
rohitsethi007 <58405577+rohitsethi007@users.noreply.github.com>
2025-05-18 21:48:21 +0100

    header layout fixed

--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -5,7 +5,7 @@ import { AuthGuardService } from '../services/auth-guard.service';
 
 const routes: Routes = [
   {
-    path: 'tabs',
+    path: '',
     component: TabsPage,
     children: [
       {
